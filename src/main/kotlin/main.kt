fcbad27a0fc06084c14f4b1a2a4c9724e91bb8a6
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-16 12:53:06 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,15 @@
 interface carPrototype {
     fun clone(): carPrototype
+    fun print()
 }
 
 class car : carPrototype {
     var name: String? = null
-    var maxSpeed: String? = null
+    var maxSpeed: Int? = null
+
+    constructor(){
+
+    }
 
     constructor(baseCar: car){
         this.name = baseCar.name
@@ -14,16 +19,43 @@ class car : carPrototype {
     override fun clone(): carPrototype {
         return car(this)
     }
+
+    override fun print(){
+        println("name: ${name}")
+        println("maxSpeed: ${maxSpeed}")
+    }
 }
 
 class carFactory{
     val cars: MutableMap<String,carPrototype> = mutableMapOf()
 
-    init{
-
+    fun getCar(name: String): carPrototype? {
+        return cars[name]
     }
+
+    init{
+        val bmw_1 = car()
+        bmw_1.name = "BMW"
+        bmw_1.maxSpeed = 200
+
+        cars["BMW_1"] = bmw_1
+
+        val bmw_2 = car()
+        bmw_1.name = "BMW"
+        bmw_1.maxSpeed = 250
+
+        cars["BMW_2"] = bmw_2
+    }
+
+
 }
 
 fun main() {
+    val carFactory = carFactory()
 
+    val bmw1 = carFactory.getCar("BMW_1")
+    val bmw2 = carFactory.getCar("BMW_2")
+
+    bmw1?.print()
+    bmw2?.print()
 }
